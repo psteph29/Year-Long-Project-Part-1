718190ee8ca2b558bb0c0f76804fa156a7176f2f
Paige Stephenson <paigestephenson29@gmail.com>
2023-02-23 10:23:17 -0700

    Initial Commit

---
+++ Tech Social Media App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tech Social Media App
+//
+//  Created by Paige Stephenson on 2/23/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
