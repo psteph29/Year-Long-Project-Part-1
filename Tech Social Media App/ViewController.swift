64c31310b5ee09458796cb468bfed926bb2be5ae
Paige Stephenson <paigestephenson29@gmail.com>
2023-02-26 22:36:49 -0700

    Add profile page and sample posts for year long project

--- Tech Social Media App/ViewController.swift
+++ Tech Social Media App/ViewController.swift
@@ -3,7 +3,16 @@
 //  Tech Social Media App
 //
 //  Created by Paige Stephenson on 2/23/23.
-//
+
+//You have joined a tech startup that needs you to start putting together a "Tech Social Media App." You need to create the user profile screen.
+//This screen should show you as a user.
+//Should show a profile photo and a background photo at the top of the screen.
+//Should show the users name and some interesting tech facts about the user(including tech interests, tech companies and tech they don't like)
+//Should have one post under the user details
+//Create a second tab where posts are displayed.
+//Show at least one post from your user
+//You are to design and create the screens yourself.
+//You are also to come up with a company name.
 
 import UIKit
 
